a9b361586a2d3830999df4895cad070384b298d1
stappost <emanuele.scarpantoni@gmail.com>
2024-03-08 17:12:57 +0100

    add route api and function show for single project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,11 +9,21 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(['technologies', 'type'])->orderBy('id', 'desc')->paginate(5);
+        $projects = Project::with(['technologies', 'type'])->orderBy('id', 'desc')->paginate(6);
 
         return response()->json([
             'success' => true,
             'results' => $projects
+        ]);
+    }
+
+    public function show($id)
+    {
+        $project = Project::all()->where('id', $id);
+        
+        return response()->json([
+            "success" => true,
+            "results" => $project
         ]);
     }
 }
